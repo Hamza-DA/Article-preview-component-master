f303219c25110899b447aa4a9c2888687017d74d
Hamza Dahmani <madebyhamzadahmani@gmail.com>
2020-12-30 23:25:00 +0100

    social share

--- App.js
+++ App.js
@@ -3,5 +3,8 @@ const shareSocial = document.querySelector('.sharesocial')
 
 shareBtn.addEventListener('click', () => {
     shareSocial.classList.toggle('active-socials')
+    shareBtn.classList.toggle('active-shareBtn')
     console.log(shareSocial);
 })
+
+// shareSocial.classList.add('active-socials')
